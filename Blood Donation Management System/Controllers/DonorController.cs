76da6c23e0ba50dc72642a8a81bc14d5f22702ad
Abdullah-Al-Amin <inabdullahalamin@gmail.com>
2026-07-25 00:05:50 +0600

    First Commit

---
+++ Blood Donation Management System/Controllers/DonorController.cs
@@ -0,0 +1,131 @@
+﻿using Blood_Donation_Management_System.EF;
+using Blood_Donation_Management_System.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blood_Donation_Management_System.Controllers
+{
+    public class DonorController : Controller
+    {
+        BloodDonationManagementSystemContext db;
+
+        public DonorController(BloodDonationManagementSystemContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Donors.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Donor donor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donors.Add(donor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donor);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var donor = db.Donors.Find(id);
+
+            if (donor == null)
+            {
+                return NotFound();
+            }
+
+            return View(donor);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Donor formObj)
+        {
+            var exObj = db.Donors.Find(formObj.DonorId);
+
+            exObj.FullName = formObj.FullName;
+            exObj.BloodGroup = formObj.BloodGroup;
+            exObj.ContactNo = formObj.ContactNo;
+            exObj.City = formObj.City;
+            exObj.LastDonationDate = formObj.LastDonationDate;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var donor = db.Donors.Find(id);
+
+            if (donor == null)
+            {
+                return NotFound();
+            }
+
+            return View(donor);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Donor formObj, string Dcsn)
+        {
+            if (Dcsn == "Yes")
+            {
+                var data = db.Donors.Find(formObj.DonorId);
+
+                db.Donors.Remove(data);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var data = db.Donors.Find(id);
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult BloodGroup(string bg)
+        {
+            var data = from d in db.Donors
+                       where string.IsNullOrEmpty(bg) || d.BloodGroup == bg
+                       select d;
+
+            ViewBag.BloodGroup = bg;
+
+            return View(data.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult RecentDonors()
+        {
+            var data = (from d in db.Donors
+                        orderby d.LastDonationDate descending
+                        select d).ToList();
+
+            return View(data);
+        }
+
+
+
+
+
+    }
+}
---
+++ Blood Donation Management System/EF/BloodDonationManagementSystemContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Blood_Donation_Management_System.EF.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blood_Donation_Management_System.EF;
+
+public partial class BloodDonationManagementSystemContext : DbContext
+{
+    public BloodDonationManagementSystemContext()
+    {
+    }
+
+    public BloodDonationManagementSystemContext(DbContextOptions<BloodDonationManagementSystemContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DbConn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.HasKey(e => e.DonationId).HasName("PK_Donation_1");
+
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.CampName).HasMaxLength(50);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup).HasMaxLength(50);
+            entity.Property(e => e.City).HasMaxLength(50);
+            entity.Property(e => e.ContactNo).HasMaxLength(50);
+            entity.Property(e => e.FullName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
